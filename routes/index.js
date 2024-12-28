dfd268ac60454fc88835cca8ea491a32cf1b2555
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-12-28 19:55:31 +0300

    feature: table styles edited

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express", companyCount: 123 });
+  res.render("index", {
+    title: "Express",
+    companyCount: 123,
+    lastUpdateDate: new Date().toDateString(),
+  });
 });
 
 module.exports = router;
