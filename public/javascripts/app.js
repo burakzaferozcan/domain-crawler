c87f8d334a7d8916d82d7e5ded759d92f7f4a182
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-12-31 23:09:00 +0300

    refactor: saved the script part in indek.ejs to a different javascript file.

---
+++ public/javascripts/app.js
@@ -0,0 +1,59 @@
+const tablBody = document.querySelector("#domain-table tbody");
+fetch("/prices", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    let domains = Object.keys(data);
+
+    document.querySelector("#domainCount").innerText = domains.length;
+
+    domains.forEach((domain) => {
+      const row = document.createElement("tr");
+      row.appendChild(createCell(domain));
+      row.appendChild(
+        createPriceCell(
+          data[domain].new_registration_fees[0]?.price,
+          data[domain].new_registration_fees[0]?.company
+        )
+      );
+      row.appendChild(
+        createPriceCell(
+          data[domain].renewal_fees[0]?.price,
+          data[domain].renewal_fees[0]?.company
+        )
+      );
+      row.appendChild(
+        createPriceCell(
+          data[domain].transfer_fees[0]?.price,
+          data[domain].transfer_fees[0]?.company
+        )
+      );
+      row.appendChild(createCell(""));
+
+      tableBody.appendChild(row);
+    });
+  });
+
+function createCell(content) {
+  const cell = document.createElement("td");
+  cell.innerHTML = content;
+  return cell;
+}
+
+function createPriceCell(price, company) {
+  let content = `
+  <b>$${price}</b> <br>
+  ${company}
+  `;
+
+  if (!price) {
+    content = "-";
+  }
+  let cell = createCell(content);
+  cell.className = "text-center";
+  return cell;
+}
