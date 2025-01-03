b2e12899f16198c4e206760d0f2d55eaf9f22aed
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-01-03 21:30:44 +0300

    feature: created getCompanyDomains function

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -78,6 +78,37 @@ function getDomainPrices(extension, date) {
     });
 }
 
+function getCompanyDomains(company, date) {
+  const tableBody = document.querySelector("#domain-table tbody");
+
+  fetch("/prices/registrar/" + company.toLowerCase() + "?date=" + date, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      let domains = Object.keys(data);
+
+      document.querySelector("#domainCount").innerText = domains.length;
+
+      domains.forEach((domain) => {
+        const row = document.createElement("tr");
+
+        row.appendChild(createCell(domain));
+        row.appendChild(
+          createPriceCell(data[domain].new_registration_fees[0]?.price)
+        );
+        row.appendChild(createPriceCell(data[domain].renewal_fees[0]?.price));
+        row.appendChild(createPriceCell(data[domain].transfer_fees[0]?.price));
+        row.appendChild(createButton("See All...", "/domain/" + domain));
+
+        tableBody.appendChild(row);
+      });
+    });
+}
+
 function createCell(content) {
   const cell = document.createElement("td");
   cell.innerHTML = content;
