bb2b1c5dc737d9b1e8609e64794a7c1012605242
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-01-03 21:25:27 +0300

    feature: created registrar route

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,48 @@
 var express = require("express");
+const Prices = require("../db/models/Prices");
+const Domains = require("../db/models/Domains");
+const Companies = require("../db/models/Companies");
+const Enum = require("../config/Enum");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
+  let date = await Prices.getLastDate();
   res.render("index", {
-    title: "Express",
+    title: "Extension List",
     companyCount: 0,
-    lastUpdateDate: new Date().toDateString(),
+    lastUpdateDate: date.toJSON(),
   });
 });
 
-router.get("/domain/:extension", function (req, res, next) {
+router.get("/domain/:extension", async function (req, res, next) {
   let extension = req.params.extension;
+
+  let domain = await Domains.findOne({ domain: extension });
+
+  let date = await Prices.getLastDate({ domain: domain._id });
+
   res.render("domain", {
-    title: `Prices for '${extension}' Extension`,
+    title: "Prices for '" + extension + "' Extension",
     extension,
     companyCount: 0,
-    lastUpdateDate: new Date().toDateString(),
+    lastUpdateDate: date.toJSON(),
+  });
+});
+
+router.get("/registrar/:company", async function (req, res, next) {
+  let companySlug = req.params.company;
+
+  let company = await Companies.findOne({
+    name: Enum.CRAWLER_TYPES[companySlug.toLowerCase()],
+  });
+
+  let date = await Prices.getLastDate({ company: company._id });
+
+  res.render("company", {
+    title: company.name + "'s Domain Extensions",
+    company: company.name,
+    lastUpdateDate: date.toJSON(),
   });
 });
 
