74a70a3a13351ec3abeeb9cf0dcfc87787ffcc57
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-01-08 22:13:32 +0300

    feature: Sitemap Creation for Google Indexing.

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   CONNECTION_STRING:
     process.env.CONNECTION_STRING || "mongodb://localhost:27017/domain-crawler",
+  BASE_URL: process.env.BASE_URL || "http://localhost:3000",
 };
